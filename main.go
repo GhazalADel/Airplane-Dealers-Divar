74b2f246b269344cf5d0a578176bdce49ee2f669
eqbal amini <eqbalamininejad@gmail.com>
2023-06-27 08:57:40 +0330

    feat: initializing project structure and requirements

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	_ "Airplane-Divar/docs"
+)
+
+//	@Title			Airplane-Divar
+//	@version		1.0
+//	@description	Quera Airplane-Divar server
+
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host						localhost:8080
+// @BasePath					/
+// @query.collection.format	multi
+func main() {
+	// server.StartServer()
+}
