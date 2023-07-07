fc99830a34bf22d9e68ddd70ca56d5221ec8045d
SaeedReza <saeedreza.alipour@gmail.com>
2023-07-07 23:31:36 +0330

    add logging interface

--- datastore/interface.go
+++ datastore/interface.go
@@ -1,1 +1,11 @@
 package datastore
+
+import (
+	"Airplane-Divar/models"
+)
+
+type LoggingDataLayer interface {
+	AddNewLogName(id uint, title string) error
+	ReportActivity(al models.ActivityLog) error
+	GetAdsActivity(id int) ([]models.ActivityLog, error)
+}
