c00fdf1c6f4bb627afc51488389d5dfbafe64a7c
SaeedReza <saeedreza.alipour@gmail.com>
2023-07-07 23:33:43 +0330

    add identity primary key and auto created_at

--- models/activity_log.go
+++ models/activity_log.go
@@ -3,14 +3,14 @@ package models
 import "time"
 
 type ActivityLog struct {
-	ID          uint      `gorm:"primary_key"`
-	CreatedAt   time.Time `gorm:"type:datetime"`
+	ID          uint      `gorm:"primaryKey;autoIncrement"`
+	CreatedAt   time.Time `gorm:"type:datetime;autoCreateTime"`
 	SubjectType string    `gorm:"type:varchar(50);not null"`
 	SubjecrID   uint      `gorm:"type:bigint;not null"`
 	CauserType  string    `gorm:"type:varchar(50)"`
 	CauserID    uint      `gorm:"type:bigint"`
 	Log         LogName   `gorm:"foreignKey:LogID"`
-	LogID       uint      `gorm:"type:bigint"`
+	LogID       uint      `gorm:"type:int"`
 	Description string    `gorm:"type:varchar(255)"`
 }
 
