e135de0015c0ee51c13b1518cc40fa979bb9b4fd
SaeedReza <saeedreza.alipour@gmail.com>
2023-07-10 20:23:13 +0330

    fix: bookmarks foreign key fix

--- database/migrations/000005_create_bookmarks_table.up.sql
+++ database/migrations/000005_create_bookmarks_table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS bookmarks (
   user_id INTEGER, 
   ad_id INTEGER,
   PRIMARY KEY (user_id, ad_id),
-  FOREIGN KEY user_id REFERENCES users(id),
-  FOREIGN KEY ad_id REFERENCES ads(id)
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (ad_id) REFERENCES ads(id)
 );
